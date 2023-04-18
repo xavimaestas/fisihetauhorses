b64cbb742107c609669812050958d5a65fee7d71
xavimaestas <102766454+xavimaestas@users.noreply.github.com>
2023-04-17 21:50:10 -0600

    header put together
    header put together, need to reduce it down to mobile to start with.

---
+++ fisihetauhorses/src/components/Header.jsx
@@ -0,0 +1,17 @@
+
+
+export default function Header(){
+    return (
+        
+        <header>
+            <div className="title_container">
+                <h1 className ="main_title">Fisihetau Horses</h1>
+                <i class="fa-solid fa-horse"></i>
+            </div>
+            
+            <i className="fa-solid fa-bars"></i>
+            <h3 className="main_phone">111-111-1111</h3>
+        </header>
+
+    )
+    }
