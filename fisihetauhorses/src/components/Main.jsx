46ce59faa6a82878ba618a7b95cb8cf016961ec1
xavimaestas <102766454+xavimaestas@users.noreply.github.com>
2023-04-18 14:40:12 -0600

    title fixes and image position
    put website name in title and put horse svg in title link. Also fixed image position.

--- fisihetauhorses/src/components/Main.jsx
+++ fisihetauhorses/src/components/Main.jsx
@@ -4,19 +4,19 @@ export default function Main(){
         <div className="main_div_component">
 
         <section className="main_content">
-            <img  className ="main_image"alt="horse running" src="https://images.unsplash.com/photo-1466495227171-d05d7e3ac2b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2458&q=80"></img>
+            <img  className="main_image"alt="horse running" src="https://images.unsplash.com/photo-1466495227171-d05d7e3ac2b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2458&q=80"></img>
             <h1 className="image_title">Equine Adventures</h1> 
         </section>
 
         <section className="all_info">
             <div className="contact_container">
                 <a className="contact_phone" href="tel:1111111111">111.111.1111</a>
-                <p>Email@email.com</p>
+                <p className="contact_email">Email@email.com</p>
             </div>
 
             <div className="company_bio">
                 <h4>The Experience</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde error consequatur eligendi beatae aperiam minima neque sint modi laudantium veritatis repudiandae ipsum quae repellat perferendis, assumenda sapiente voluptatibus officia tenetur!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas consequuntur labore itaque sunt fugiat. Optio aliquam doloribus deleniti ab ullam placeat minima, dolorum amet odio laboriosam quo corrupti molestias ea veniam deserunt est nam odit temporibus! Voluptas omnis molestiae optio quam magnam perferendis quo laudantium unde quibusdam nostrum placeat quia modi, eaque sed laborum sunt itaque quisquam repudiandae illum rem nulla. Molestiae recusandae ipsa sint nisi, non aliquid? Modi exercitationem repellat, laudantium animi aperiam dolorum quae veniam architecto eveniet, atque repudiandae, eaque eum? Iste possimus consequuntur veniam aut, praesentium magnam minus sed minima reiciendis hic, sunt laborum fuga voluptatibus ipsa!</p>
             </div>
 
         </section>
