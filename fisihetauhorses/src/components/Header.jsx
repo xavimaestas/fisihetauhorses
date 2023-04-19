b609b1f6ef4ec5011098d2c8c35b9b95a8d3d8ca
xavimaestas <102766454+xavimaestas@users.noreply.github.com>
2023-04-19 13:36:18 -0600

    media query/text
    media query and text updated

--- fisihetauhorses/src/components/Header.jsx
+++ fisihetauhorses/src/components/Header.jsx
@@ -5,14 +5,26 @@ export default function Header(){
         
         <header>
             <div className="header_container">
+               
+
+
                 <div className="title_container">
                     <h1 className ="main_title">Fisihetau Horses</h1>
-                    <i class="fa-solid fa-horse fa-lg"></i>
+                    <i className="fa-solid fa-horse fa-lg"></i>
                 </div>
-                <h3 className="main_phone">111-111-1111</h3>
+                <a className="main_phone" href="tel:111-111-1111">111-111-1111</a>
             </div>
             
+            
+            <div className="drop_down">
             <i className="fa-solid fa-bars"></i>
+                <div className="drop_down_content">
+                    <a href="#experience">The Experience</a>
+                    <br></br>
+                    <a href="">Contact</a>
+                </div>
+                        
+                </div>
             
         </header>
 
