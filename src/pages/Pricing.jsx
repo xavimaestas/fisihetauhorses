63708e7aee38d291100da29b496e2ae1c62e4499
xavimaestas <xavimaestas@gmail.com>
2023-07-21 15:03:48 -0600

    fh card started
    adventure ride card created

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -5,19 +5,33 @@ export default function Pricing(){
 
         <div className="page-top-margin pricing-container">
 
-        <img className="price-img" alt="close-up of two horses" src={newbg}></img>
 
-        <div className="pricing-text">
-            <h3>Pricing</h3>
+        <div className="fareharbor-card">
+            <img className="fareharbor-img" alt="close-up of two horses" src={newbg}></img>
+            <h3 className="fareharbor-title">Adventure Ride and Horsemanship Lesson</h3>
+            {/* <p className="fareharbor-text" >Activity Text</p> */}
+
+        
+
+            <a className="fareharbor-btn" href="https://fareharbor.com/embeds/book/fisihetauhorses/items/480554/?full-items=yes&flow=1006891">Book</a>
+
+         
+
+          
+          
+
+        </div>
+
+       
             
             <ul className="contact_main">
                         <li><strong>Phone:</strong> (801) 815-2573</li>
                         <li><strong>Email:</strong> snfisihetau@gmail.com</li>
                     </ul>
-            <a  href="https://fareharbor.com/embeds/book/fisihetauhorses/items/480785/?full-items=yes&flow=1006891">Fareharbor</a>
+         
 
-        </div>
-        <div><script src="https://fareharbor.com/embeds/script/items/fisihetauhorses/?full-items=yes&amp;fallback=simple&flow=1006891"></script></div>
+        
+       
      
 
       
