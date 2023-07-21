847e6b59b7f20c6836ae0bc99b903708e8ca0f5a
xavimaestas <xavimaestas@gmail.com>
2023-07-20 23:08:41 -0600

    Home, Meet, About
    Home, Meet the Horses and About pages approaching finish. Next step is working on booking cards with fareharbor.

--- src/pages/AboutOwner.jsx
+++ src/pages/AboutOwner.jsx
@@ -1,21 +1,40 @@
 import owner1 from '../assets/owner1.jpg'
+import samCanyon from '../assets/sam-canyon.jpeg'
+import { HashLink } from 'react-router-hash-link'
 
 export default function AboutOwner(){
     return (
 
-    <div className=" about-owner-container">
+        <>
 
-        <img className="owner1" alt="man with cowboy hat" src={owner1}></img>
+        <div className="about-owner-desktop">
 
-        <div className="about-owner-text">
-            <h4>About The Owner</h4>
-            <p>Sam is a first generation horseman. He spent two seasons in East Zion and one season in Park City, taking horseback rides through some of Utah's most impressive landscapes. He is an experienced all terrain horseman. Traveling through terrain in all seasons of the year. Sam is a licensed real estate professional and specializes in commerical investment properties and homes for his family and friends. The word "genuine" describes him well. With Sam you will have a personal experience everytime. </p>
+            <div className="about-owner-container">
+
+                <img className="owner1" alt="man with cowboy hat" src={owner1}></img>
+                <HashLink to='#about-owner-text' ><i class="fa-solid fa-angles-down about-owner-angles fa-2xl"></i></HashLink>
+
+                <div className="about-owner-text" id="about-owner-text">
+                    <h4 >About The Owner</h4>
+                    <p>Sam is a first generation horseman. He spent two seasons in East Zion and one season in Park City, taking horseback rides through some of Utah's most impressive landscapes. He is an experienced all terrain horseman. Traveling through terrain in all seasons of the year. Sam is a licensed real estate professional and specializes in commerical investment properties and homes for his family and friends. The word "genuine" describes him well. With Sam you will have a personal experience everytime. </p>
+                </div>
+
+                
+
+            </div>
+
+            <img className="owner2" alt="man with cowboy hat" src={samCanyon}></img>
+                
+         
+    
         </div>
 
-     
+    
 
         
-    </div>
+        </>
+  
+    
       
         
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,10 @@ import samRiding1 from '../assets/sam-riding1.mp4'
 import samRiding2 from '../assets/sam-riding2.mp4'
 import samRiding4 from '../assets/sam-riding4.mp4'
 import samRiding5 from '../assets/sam-riding5.mp4'
+import samRiding1Ph from '../assets/samRiding1Ph.jpg'
+import samRiding2Ph from '../assets/samRiding2Ph.jpg'
+import samRiding4Ph from '../assets/samRiding4Ph.jpg'
+import samRiding5Ph from '../assets/samRiding5Ph.jpg'
 
 
 export default function Main(){
@@ -12,40 +16,40 @@ export default function Main(){
 
     <div className="main_div_component">
 
-        <section className="main_content">
-            <img  className="main_image"alt="close up of two horses" src={newBg}></img>
-            <h1 className="image_title"></h1> 
-        </section>
+    
 
         <section className="all_info" id="all_info">
             
 
             <div className="bio_container" id="bio_container">
-                <div className="experience" id="experience">
-
+                {/* <div className="experience" id="experience">
+                    <img  className="main_image"alt="close up of two horses" src={newBg}></img>
                     <div className="experience_text">
-                    <h4>Let's Ride!</h4>
-                    <p>Join us for an authentic horse back experience in the majestic landscapes of Utah. Learn how to care, saddle and ride in a private and genuine setting. Book an advanced lesson or event and invite these horses to your party or proposal.</p>
+                        <h4>Let's Ride!</h4>
+                        <p>Join us for an authentic horse back experience in the majestic landscapes of Utah. Learn how to care, saddle and ride in a private and genuine setting. Book an advanced lesson or event and invite these horses to your party or proposal.</p>
                     </div>
 
-                    <div className="experience_text">
-                    
-
-                    </div>
                    
-                    {/* <img className="sam_horse " alt="horse" src={samHorse}></img> */}
-                </div>
+                    <img className="sam_horse " alt="horse" src={samHorse}></img>
+                </div> */}
                 <div className="horse_container">
 
                     <div className="video_container" id="video_container">
 
-                    <video className="sam_riding_v1 content" preload="auto" loop muted controls src={samRiding1}></video>
+                    <img  className="sam_riding_v1"alt="close up of two horses" src={newBg}></img>
 
-                    <video className="sam_riding_v1 content video_margin" src={samRiding5} preload="auto" loop muted="muted" controls="controls"></video>
+                    <div className="experience_text">
+                        <h4>Let's Ride!</h4>
+                        <p>Join us for an authentic horse back experience in the majestic landscapes of Utah. Learn how to care, saddle and ride in a private and genuine setting. Book an advanced lesson or event and invite these horses to your party or proposal.</p>
+                    </div>
 
-                    <video className="sam_riding_v1 content video_margin" src={samRiding4} preload="auto" loop muted="muted" controls="controls"></video>
+                    <video className="sam_riding_v1 content" preload="auto" loop muted controls src={samRiding1} poster={samRiding1Ph}></video>
 
-                    <video className="sam_riding_v1 content video_margin" src={samRiding2} preload="auto" loop muted="muted" controls="controls"></video>
+                    <video className="sam_riding_v1 content video_margin" src={samRiding4} poster={samRiding4Ph}preload="auto" loop muted="muted" controls="controls"></video>
+
+                    <video className="sam_riding_v1 content video_margin" src={samRiding5} poster={samRiding5Ph} preload="auto" loop muted="muted" controls="controls"></video>
+
+                    <video className="sam_riding_v1 content video_margin" src={samRiding2} poster={samRiding2Ph} preload="auto" loop muted="muted" controls="controls"></video>
                         
                     </div>
 
