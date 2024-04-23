a7912b53e21ea9115b56a2c62bbbcad24d4b4a3f
xavimaestas <102766454+xavimaestas@users.noreply.github.com>
2024-04-23 07:56:19 -0600

    text updated
    pricing text updated and pricing match

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -17,38 +17,68 @@ export default function Pricing(){
 
         <div className="card-container">
         <Card 
-                    title="Fundamentals Lesson"
-                    text="Discover an exciting equestrian adventure with our 1-hour Beginner Horseback Riding Lesson. Perfect for newcomers, our skilled instructors guide you through mounting, commands, and essential riding skills, fostering confidence and a harmonious connection with your horse."
+                    title="Fundamentals Horseback Riding Lesson"
+                    text={<div>Embark on an exciting equestrian adventure with our Fundamental Riding Lesson. Designed for those new to horseback riding, this immersive experience offers a safe and supportive environment to learn the basics of horsemanship. Our skilled instructor(s) will guide you through saddling, proper mounting techniques, basic commands, and essential riding skills, fostering confidence and a harmonious connection with your equine partner.
+                    <br/>
+                    <br/>
+                    Duration: Approximately 1 Hour
+                    <br/>
+                    <br/>
+                    Please Note: Maximum capacity of 2 riders, single rider bookings will be charged an additional $25. Age minimum +6 and weight limit of 250lbs. All minors must be accompanied by an adult.</div>}
                     image={samcanyon}
                     btn={"https://fareharbor.com/embeds/book/fisihetauhorses/items/480734/?full-items=yes&flow=1006891"}
-                    price={"$75"}
+                    price={"$79.50"}
                     imageStyle="samcanyon"
                 />
         <Card 
                     title="Adventure Ride & Horsemanship Lesson"
-                    text="The All Terrain lesson will start with an hour refresher saddling getting to know your horse, and going over the basics, before setting off on our adventure. The remaining 2 hours will be spent learning to ride and exploring the surrounding area by horseback."
+                    text={<div>Discover the bliss of horseback riding. Learn how to care for, saddle, and ride these majestic creatures in native terrains and in all seasons of the year. Perfect for special occasions, anniversaries, or just a great time with your friend. Our skilled instructor(s) will guide you through saddling, proper mounting techniques, basic commands, and essential riding skills, fostering confidence and a harmonious connection with your equine partner.
+                    <br/>
+                    <br/>
+                    Duration: Approximately 3 Hours
+                    <br/>
+                    <br/>
+                    Please Note: Maximum capacity of 2 riders. Single rider bookings will be charged an additional $50. Age minimum +14 and weight limit of 250lbs. All minors must be accompanied by an adult.
+
+                    </div>}
                     image={meethorses2}
                     btn={"https://fareharbor.com/embeds/book/fisihetauhorses/items/480767/calendar/2023/07/?full-items=yes&flow=1006891"}
-                    price={"$150"}
+                    price={"$159"}
                     
                 />
 
         <Card 
-                    title="Horse Companions for Unforgettable Events"
-                    text="Immerse in enchanting Horse Companionship - perfect for children's parties! Young ones aged 2+ will cherish quality time with gentle, majestic equine friends. Capture precious moments through photos in a safe, interactive environment without riding."
+                    title="Invite Horse(s) to your Event"
+                    text={<div>This unique offering focuses on creating a calm and          interactive environment where children and adults alike can be or sit on a gentle horse. It's a great option for those who wish to engage with these magnificent creatures in a more relaxed setting without riding independently. Your guests can also be hand-led around via horseback. Book these horses for your next event!
+                        <br/>
+                        <br/>
+                        Duration: 1 Hour
+                        <br/>
+                        <br/>
+                        Please Note: Base rate for 1 horse, for additional horses or a longer duration please call for additional pricing. Age minimum +2 and weight limit of 250lbs. All minors must be accompanied by an adult.
+    
+                        </div>}
                     image={meethorses1}
                     btn={"https://fareharbor.com/embeds/book/fisihetauhorses/items/480554/?full-items=yes&flow=1006891"}
-                    price={"$250"}
+                    price={"$265"}
                     imageStyle="meethorses1"
                 />
                 
                
                 <Card 
                     title="Horseback Proposal"
-                    text="Amidst picturesque landscapes, combine horseback riding with a heartfelt proposal. Seize the perfect moment to profess your love and create an unforgettable memory cherished for a lifetime."
+                    text={<div>Ignite the spark of romance with our breathtaking horseback ride proposal experience. Set amidst picturesque landscapes, this enchanting journey combines the thrill of horseback riding with the ecstasy of a heartfelt proposal. Seize the perfect moment and create an unforgettable memory to be cherished for a lifetime. You Can't Get More Charming Than This!
+                    <br/>
+                    <br/>
+                    Duration: Approximately 3 Hours
+                    <br/>
+                    <br/>
+                    Please Note: Maximum capacity of 2 riders. Age minimum +18 and weight limit of 250lbs. Photographer not included, but available upon request. Call for additional pricing.
+
+                    </div>}
                     image={newbg}
                     btn={"https://fareharbor.com/embeds/book/fisihetauhorses/items/480785/?full-items=yes&flow=1006891"}
-                    price={"$500"}
+                    price={"$530"}
                 />
                 
                 <Card 
